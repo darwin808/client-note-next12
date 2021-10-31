641596eff1aaa2575ea17dcb91af11814609ffb4
darwin <darwin.apolinario@serino.com>
2021-10-31 11:23:58 +0800

    feat(card.tsx): add card tsx
    laskdjfal;ksdjflk jsl;dkfjlask jflaskjdf lkajsdlf jasl;dkfj al;skdjf laksjd flkjsal fkjaslkdfjs

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Card = () => {
+  return (
+    <div className="w-96 h-52 bg-yellow-300  flex  flex-col ">
+      <input placeholder="name " type="text" />
+      <textarea name="" id="" cols={30} rows={10}></textarea>
+    </div>
+  )
+}
+
+export default Card
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next"
+import Card from "../components/Card"
 
 const Home: NextPage = () => {
-  return <h1>darwi123123n</h1>
+  return (
+    <div className="bg-red-300 h-screen w-full ">
+      <h1>title</h1>
+
+      <div className="NoteContainer">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  )
 }
 
 export default Home
