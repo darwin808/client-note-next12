688d6dde7fe53efffc5584a43a38d86e8771e5ec
darwin <darwin.apolinario@serino.com>
2021-10-31 10:43:50 +0800

    initialize server ;

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return <h1>darwin</h1>;
+};
+
+export default Home;
